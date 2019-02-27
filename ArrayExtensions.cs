d656cb2b4edb3bbe6cc14c9c632a81191baaea80
GlennManaker <vova_f@inbox.ru>
2019-02-27 19:42:28 +0300

    dd

---
+++ ArrayExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PadawansTask11
+{
+    public static class ArrayExtensions
+    {
+        public static int? FindIndex(double[] array, double accuracy)
+        {
+            if (accuracy < 0 || accuracy > 1)
+            {
+                throw new ArgumentException();
+            }
+
+            if (array == null)
+            {
+                return null;                                                            
+            }
+
+            double sumRight = 0;
+
+            double sumLeft = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumRight += array[i];
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumRight -= array[i];
+
+                if (Math.Abs(sumRight - sumLeft) < accuracy)
+                {
+                    return i;
+                }
+
+                sumLeft += array[i];
+            }
+
+            return null;
+        }
+    }
+}
